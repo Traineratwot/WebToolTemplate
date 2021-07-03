a0ceeb1b7e2fe652c0d3aaa51e2d2ec49f274c55
Kirill <41589091+Traineratwot@users.noreply.github.com>
2021-07-03 19:38:48 +0300

    Update core.php

--- core/core.php
+++ core/core.php
@@ -56,8 +56,8 @@
 		public $isNew = TRUE;
 		public $schema = [];
 		public $update = [];
-		public $data= [];
-		public $_fields= [];
+		public $data = [];
+		public $_fields = [];
 
 		public function __construct(Core &$core, $where = [])
 		{
@@ -370,23 +370,5 @@
 				}
 			}
 			return FALSE;
-		}
-
-		public static function convert_from_latin1_to_utf8_recursively($dat)
-		{
-			if (is_string($dat)) {
-				return utf8_encode($dat);
-			} elseif (is_array($dat)) {
-				$ret = [];
-				foreach ($dat as $i => $d) $ret[$i] = self::convert_from_latin1_to_utf8_recursively($d);
-
-				return $ret;
-			} elseif (is_object($dat)) {
-				foreach ($dat as $i => $d) $dat->$i = self::convert_from_latin1_to_utf8_recursively($d);
-
-				return $dat;
-			} else {
-				return $dat;
-			}
 		}
 	}
