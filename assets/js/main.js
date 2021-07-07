511fc450693f067bc8c284bfa07fe8c4541195cd
Kirill <41589091+Traineratwot@users.noreply.github.com>
2021-07-07 19:28:04 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,35 +2,59 @@ var Core = /** @class */ (function () {
     function Core() {
         this.events();
     }
+    Core.prototype.id = function (length) {
+        if (length === void 0) { length = 6; }
+        length = length - 1;
+        var result = 'a';
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for (var i = 0; i < length; i++) {
+            result += characters.charAt(Math.floor(Math.random() *
+                charactersLength));
+        }
+        return result;
+    };
     Core.prototype.events = function () {
+        var Core = this;
         $('form').on('submit', function (e) {
-            var _a;
             if (this instanceof HTMLFormElement) {
-                var formData = new FormData(this);
                 var form = $(this);
-                var method = (_a = form.attr('method')) !== null && _a !== void 0 ? _a : 'POST';
-                var action = form.attr('action');
-                var id = form.attr('id');
-                if (method && action && id) {
-                    e.preventDefault();
-                    $.ajax({
-                        type: method,
-                        url: action,
-                        cache: false,
-                        contentType: false,
-                        processData: false,
-                        data: formData,
-                        dataType: 'json',
-                        success: function (msg) {
-                            msg.formId = id;
-                            if (msg.success == true) {
-                                form.trigger('success', msg);
+                var formData = new FormData(this);
+                var c = form.trigger('beforeSubmit', formData);
+                if (c) {
+                    var method = form.attr('method') || 'POST';
+                    var action = form.attr('action');
+                    var dataType = form.data('type') || 'json';
+                    var id = form.attr('id') || Core.id();
+                    if (method && action) {
+                        e.preventDefault();
+                        $.ajax({
+                            type: method,
+                            url: action,
+                            cache: false,
+                            contentType: false,
+                            processData: false,
+                            data: formData,
+                            dataType: dataType,
+                            success: function (msg) {
+                                msg.formId = id;
+                                if (msg.success == true) {
+                                    form.trigger('success', msg);
+                                }
+                                else {
+                                    form.trigger('failure', msg);
+                                }
+                            },
+                            error: function (e) {
+                                // @ts-ignore
+                                e.success = false;
+                                form.trigger('failure', e);
                             }
-                            else {
-                                form.trigger('failure', msg);
-                            }
-                        }
-                    });
+                        }).done(function (e) {
+                            e.success = false;
+                            form.trigger('submit', e);
+                        });
+                    }
                 }
             }
         });
