2f290b5d854dbfaf84e1bf83883006f516da0b7b
Kirill <41589091+Traineratwot@users.noreply.github.com>
2021-07-09 00:10:38 +0300

    Update engine.php

--- core/model/engine.php
+++ core/model/engine.php
@@ -1,13 +1,12 @@
 <?php
 
 	use core\model\Core;
-
-	require_once realpath(WT_MODEL_PATH . 'core.php');
-	require_once realpath(WT_MODEL_PATH . 'errors.php');
-	require_once realpath(WT_VENDOR_PATH . 'autoload.php');
 	if (WT_TYPE_DB == 'sqlite') {
 		error_reporting(E_ALL ^ E_WARNING);
 	}
+	require_once realpath(WT_MODEL_PATH . 'core.php');
+	require_once realpath(WT_MODEL_PATH . 'errors.php');
+	require_once realpath(WT_VENDOR_PATH . 'autoload.php');
 	$classes = scandir(WT_CLASSES_PATH);
 	foreach ($classes as $class) {
 		$p = realpath(WT_CLASSES_PATH . $class);
