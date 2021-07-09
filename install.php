ca696a4f29d23000a82279f3f816c9dd0b4c899e
Kirill <41589091+Traineratwot@users.noreply.github.com>
2021-07-09 13:26:00 +0300

    Update install.php

--- install.php
+++ install.php
@@ -26,15 +26,22 @@
 	if (!file_exists(WT_BASE_PATH . 'node_modules')) {
 		exec('npm update');
 	}
+	if (!file_exists(WT_BASE_PATH . 'node_modules')) {
+		echo "Не удалось установить npm";
+	}
 	if (!file_exists(WT_VENDOR_PATH . 'autoload.php')) {
 		exec('cd '.WT_CORE_PATH );
 		exec('composer update');
 	}
-	require_once realpath(WT_MODEL_PATH . 'engine.php');
-	if (util::getSystem() == 'nix') {
-		exec('chmod 755 -R -f '.WT_MODEL_PATH);
-		exec('chmod 744 -R -f '.WT_CORE_PATH.'config.php');
-		exec('chmod 744 -R -f '.WT_CORE_PATH.'config.json');
-		exec('chmod 766 -R -f '.WT_AJAX_PATH);
+	if (!file_exists(WT_BASE_PATH . 'node_modules')) {
+		echo "Не удалось установить composer";
+	}else{
+		require_once realpath(WT_MODEL_PATH . 'engine.php');
+		if (util::getSystem() == 'nix') {
+			exec('chmod 755 -R -f '.WT_MODEL_PATH);
+			exec('chmod 744 -R -f '.WT_CORE_PATH.'config.php');
+			exec('chmod 744 -R -f '.WT_CORE_PATH.'config.json');
+			exec('chmod 766 -R -f '.WT_AJAX_PATH);
+		}
 	}
 	rename(__FILE__,__FILE__.'.txt');
