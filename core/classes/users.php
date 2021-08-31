bbfa718fcbe909aaef55ac2133498a76a19130e1
Kirill <41589091+Traineratwot@users.noreply.github.com>
2021-08-31 16:42:12 +0300

    Update users.php

--- core/classes/users.php
+++ core/classes/users.php
@@ -13,8 +13,26 @@
 		public $table = 'users';
 		public $primaryKey = 'id';
 
-		public function sendMail($subject, $body='', $file = [])
+		public function sendMail($subject, $body = '', $file = [])
 		{
 			return $this->core->mail($this, $subject, $body, $file);
+		}
+
+		/**
+		 * Get either a Gravatar URL or complete image tag for a specified email address.
+		 *
+		 * @param string $email The email address
+		 * @param string $s     Size in pixels, defaults to 80px [ 1 - 2048 ]
+		 * @param string $d     Default imageset to use [ 404 | mp | identicon | monsterid | wavatar ]
+		 * @param string $r     Maximum rating (inclusive) [ g | pg | r | x ]
+		 * @return String containing either just a URL or a complete image tag
+		 * @source https://gravatar.com/site/implement/images/php/
+		 */
+		function getGravatar($s = 80, $d = 'mp', $r = 'g')
+		{
+			$url = 'https://www.gravatar.com/avatar/';
+			$url .= md5(strtolower(trim($this->get('email'))));
+			$url .= "?s=$s&d=$d&r=$r";
+			return $url;
 		}
 	}
