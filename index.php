c0fc8baa839cb38b1a1bb268d7e1751d0c731047
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-02-19 12:04:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,6 @@
 
 	namespace index;
 	session_start();
-	require_once realpath(__DIR__ . '/core/__config.php');
 	require_once realpath(__DIR__ . '/core/config.php');
 	require_once realpath(WT_MODEL_PATH . 'engine.php');
 	require_once realpath(WT_MODEL_PATH . 'page/router.php');
