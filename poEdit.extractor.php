b60d2039ef49ae05b8963fec9823d6ee60c42f30
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-04-22 16:05:24 +0300

    Update poEdit.extractor.php

--- poEdit.extractor.php
+++ poEdit.extractor.php
@@ -2,12 +2,11 @@
 
 	namespace index;
 
-	use model\Console;
-	use model\PoUpdate;
+
+	use model\locale\PoUpdate;
 
 	require_once realpath(__DIR__ . '/core/config.php');
 	require_once realpath(WT_MODEL_PATH . 'engine.php');
-	Console::prompt('TEST');
 
 	file_put_contents(__DIR__.'/log.log',json_encode($argv));
 
