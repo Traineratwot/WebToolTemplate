2c7b1b7d467a10ce5830c3a53522788a253a8869
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-04-29 17:29:33 +0300

    Update CronCmd.php

--- core/model/cli/commands/CronCmd.php
+++ core/model/cli/commands/CronCmd.php
@@ -3,6 +3,7 @@
 	namespace model\cli\commands;
 
 	use model\cli\types\FilePath;
+	use model\main\Utilities;
 	use Traineratwot\PhpCli\Cmd;
 	use Traineratwot\PhpCli\Console;
 	use Traineratwot\PhpCli\types\TBool;
@@ -23,25 +24,35 @@
 			if (!defined('WT_PHP_EXEC_CMD')) {
 				define('WT_PHP_EXEC_CMD', 'php');
 			}
-			$path    = $this->getArg('path');
-			$cmd     = $this->getArg('cmd') ?: WT_PHP_EXEC_CMD;
-			$run     = $this->getArg('run');
-			$command = $cmd . ' ' . WT_CRON_PATH . 'launch.php -f"' . $path . '"';
-			if (!is_null($run)) {
-				if ($run === TRUE) {
-					$command .= ' -d true';
+			$path = $this->getArg('path');
+			if (is_null($path)) {
+				$array_cron = Utilities::glob(WT_CRON_PATH . 'controllers/', '*.*');
+				Console::success('Список кронов');
+				$s = Utilities::pathNormalize(WT_CRON_PATH . 'controllers/', '/');
+				foreach ($array_cron as $cron) {
+					$cron = Utilities::pathNormalize($cron, '/');
+					Console::info('    ' . str_replace($s, '', $cron));
 				}
-				exec($command, $out);
-				echo implode("\n", $out) . PHP_EOL;
 			} else {
-				echo Console::getColoredString($command, 'green');
+				$cmd     = $this->getArg('cmd') ?: WT_PHP_EXEC_CMD;
+				$run     = $this->getArg('run');
+				$command = $cmd . ' ' . WT_CRON_PATH . 'launch.php -f"' . $path . '"';
+				if (!is_null($run)) {
+					if ($run === TRUE) {
+						$command .= ' -d true';
+					}
+					exec($command, $out);
+					echo implode("\n", $out) . PHP_EOL;
+				} else {
+					echo Console::getColoredString($command, 'green');
+				}
 			}
 		}
 
 		public function setup()
 		{
-			$this->registerParameter('path', 1, FilePath::class, "Относительный путь  от папки 'controllers' до файла задания");
-			$this->registerOption('cmd', 'c', 0, TString::class, "Введите CMD коммаду для запуска файла по умодчанию 'WT_PHP_EXEC_CMD' ");
+			$this->registerParameter('path', 0, FilePath::class, "Относительный путь  от папки 'controllers' до файла задания");
+			$this->registerOption('cmd', 'c', 0, TString::class, "Введите CMD команду для запуска файла по умолчанию 'WT_PHP_EXEC_CMD' ");
 			$this->registerOption('run', 'r', 0, TBool::class, "Установите этот флаг чтобы запустить задание");
 		}
 	}
