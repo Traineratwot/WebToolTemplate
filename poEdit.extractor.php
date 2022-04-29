673ebbfe2e133c365c231c46088055322b87f8f1
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-04-29 23:06:20 +0300

    Update poEdit.extractor.php

--- poEdit.extractor.php
+++ poEdit.extractor.php
@@ -7,7 +7,4 @@
 
 	require_once __DIR__ . '/core/config.php';
 	require_once WT_MODEL_PATH . 'engine.php';
-
-	file_put_contents(__DIR__ . '/log.log', json_encode($argv));
-
 	(new PoUpdate())->poEdit();
