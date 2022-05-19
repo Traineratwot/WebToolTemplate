819a9b47822f5e8b504a45d51e2dcc298d87222c
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-05-19 22:42:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,9 @@
 <?php
 
 	namespace index;
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
 
 	use model\main\Core;
 
-	ini_set('display_errors', 1);
-	error_reporting(E_ALL);
 	session_start();
 	require_once __DIR__ . '/vendor/autoload.php';
 	Core::init();
