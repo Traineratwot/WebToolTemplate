2940962c03559d674623319afda9c2c3b0a2ed58
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-05-26 20:56:17 +0300

    update

--- core/composer/Scripts.php
+++ core/composer/Scripts.php
@@ -15,9 +15,9 @@
 	class Scripts
 	{
 		public static function postInstall()
+		: void
 		{
 			require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
-
 			$config = get_defined_constants();
 			Console::info('Create missing directory');
 			foreach ($config as $v => $i) {
@@ -25,7 +25,7 @@
 					self::mkDirs($i);
 				}
 			}
-			if (PHP_OS === "Linux") {
+			if (PHP_OS === "Linux" && self::commandExist('exec')) {
 				Console::info('Change permission');
 				exec('chmod 755 -R -f ' . WT_MODEL_PATH);
 				exec('chmod 744 -R -f ' . WT_CORE_PATH . 'config.php');
@@ -42,7 +42,7 @@
 						if (!file_exists(WT_HOST_DB)) {
 							file_put_contents(WT_HOST_DB, '');
 						}
-						$core->db->exec(<<<SQL
+						$core->db->exec(<<<HTML
 CREATE TABLE users
 (
 	id       INTEGER
@@ -54,10 +54,10 @@ CREATE TABLE users
 	authKey  VARCHAR(64),
 	salt     VARCHAR(64) DEFAULT 0
 );
-SQL
+HTML
 						);
 					} else {
-						$core->db->exec(<<<SQL
+						$core->db->exec(<<<HTML
 CREATE TABLE `users` (
 	`id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`email` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_unicode_ci',
@@ -71,9 +71,8 @@ CREATE TABLE `users` (
 )
 COLLATE='utf8mb4_unicode_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=0
-;
-SQL
+AUTO_INCREMENT=0;
+HTML
 						);
 					}
 				}
@@ -81,54 +80,83 @@ SQL
 			} catch (Exception $e) {
 				Console::failure($e->getMessage());
 			}
-			Console::info('npm install');
-			chdir(WT_BASE_PATH);
-			exec('npm install');
-			return TRUE;
+			if (self::commandExist('npm')) {
+				Console::info('npm install');
+				chdir(WT_BASE_PATH);
+				exec('npm install');
+			} else {
+				Console::warning('Please install npm');
+			}
+			die;
 		}
 
 		public static function engineUpdate()
+		: void
 		{
+			require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+			self::mkDirs(WT_BASE_PATH . 'update');
+			chdir(WT_BASE_PATH . 'update');
+			exec('git clone "https://github.com/Traineratwot/WebToolTemplate.git" .');
+			self::copy(WT_BASE_PATH . 'update/core/model', WT_MODEL_PATH);
+			self::rmdir(WT_BASE_PATH . 'update/');
+			die;
 
+		}
+
+		private static function commandExist($cmd)
+		: bool
+		{
+			if (PHP_OS === "Linux") {
+				$return = shell_exec(sprintf("which %s", escapeshellarg($cmd)));
+				return !empty($return);
+			}
+			exec(sprintf("where %s", escapeshellarg($cmd)), $r, $c);
+			if ($c !== 0) {
+				return FALSE;
+			}
+			$r = shell_exec(sprintf("help %s", escapeshellarg($cmd)));
+			return strpos('not supported', $r) === FALSE;
 		}
 
 		/**
 		 * @throws ZipException
 		 */
 		public static function package()
+		: void
 		{
+			//------------------------------DATABASE-------------------------------------
 			$dt = new DateTime();
 			require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 			self::mkDirs(WT_BASE_PATH . '/backups/');
-
-
 			$zipFile = new ZipFile();
-			$zipFile->setArchiveComment(<<<TXT
+			$zipFile->setArchiveComment(<<<HTML
 Backup DataBase
 Date: {$dt->format(DATE_ATOM)}
-TXT
+HTML
 			);
 			switch (WT_TYPE_DB) {
 				case PDOE::DRIVER_MySQL:
-					$h = WT_HOST_DB;
-					$p = WT_PASS_DB;
-					$u = WT_USER_DB;
-					$d = WT_DATABASE_DB;
-					$s = WT_CORE_PATH . 'database/dump.sql';
-					self::mkDirs(dirname($s));
-					$cmd = <<<BASH
+					if (self::commandExist('mysqldump')) {
+						$h = WT_HOST_DB;
+						$p = WT_PASS_DB;
+						$u = WT_USER_DB;
+						$d = WT_DATABASE_DB;
+						$s = WT_CORE_PATH . 'database/dump.sql';
+						self::mkDirs(dirname($s));
+						$cmd = <<<HTML
 mysqldump -u $u -p$p -h $h $d > "$s"
-BASH;
-					Console::info($cmd);
+HTML;
+						exec($cmd, $o, $c);
+						if ($c === 0) {
+							$zipFile->addFile($s);
 
-					exec($cmd, $o, $c);
-					if ($c === 0) {
-						$zipFile->addFile($s);
-
-						$zipFile->saveAsFile(WT_BASE_PATH . '/backups/dump.zip');
-						$zipFile->close();
+							$zipFile->saveAsFile(WT_BASE_PATH . '/backups/dump.zip');
+							$zipFile->close();
+						} else {
+							Console::warning("You must make dump, use: \"$cmd\"");
+						}
 					} else {
-						Console::warning("You must make dump, use: \"$cmd\"");
+						Console::warning("failed to make a backup copy of the database");
 					}
 					break;
 				case PDOE::DRIVER_PostgreSQL:
@@ -140,14 +168,12 @@ BASH;
 					$zipFile->close();
 					break;
 			}
-
-			//--------------------------------------------------
-
+			//---------------------------------CORE--------------------------------------
 			$zipFile = new ZipFile();
-			$zipFile->setArchiveComment(<<<TXT
+			$zipFile->setArchiveComment(<<<HTML
 Backup project
 Date: {$dt->format(DATE_ATOM)}
-TXT
+HTML
 			);
 			$zipFile->addDirRecursive(WT_BASE_PATH);
 			$zipFile->deleteFromRegex("@.idea@");
@@ -156,9 +182,11 @@ TXT
 			$zipFile->deleteFromRegex("@backups@");
 			$zipFile->saveAsFile(WT_BASE_PATH . '/backups/backup.zip');
 			$zipFile->close();
+			die;
 		}
 
-		public static function mkDirs(string $dir)
+		private static function mkDirs(string $dir)
+		: void
 		{
 			$dir = Utilities::pathNormalize($dir);
 			if (!file_exists($dir)) {
@@ -167,6 +195,39 @@ TXT
 				} else {
 					Console::success(sprintf('Created "%s"', $dir));
 				}
+			}
+		}
+
+		private static function rmdir($dir)
+		{
+			if (is_dir($dir)) {
+				$files = scandir($dir);
+				foreach ($files as $file) {
+					if ($file !== "." && $file !== "..") {
+						self::rmdir("$dir/$file");
+					}
+				}
+				rmdir($dir);
+			} elseif (file_exists($dir)) {
+				unlink($dir);
+			}
+		}
+
+		private static function copy($src, $dst)
+		{
+			if (file_exists($dst)) {
+				self::rmdir($dst);
+			}
+			if (is_dir($src)) {
+				self::mkdirs($dst);
+				$files = scandir($src);
+				foreach ($files as $file) {
+					if ($file !== "." && $file !== "..") {
+						self::copy("$src/$file", "$dst/$file");
+					}
+				}
+			} elseif (file_exists($src)) {
+				copy($src, $dst);
 			}
 		}
 	}
