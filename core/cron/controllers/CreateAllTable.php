c1db764f26c7d06a4d4946e14864d0368132525f
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-06-06 11:09:48 +0300

    Update CreateAllTable.php

--- core/cron/controllers/CreateAllTable.php
+++ core/cron/controllers/CreateAllTable.php
@@ -1,22 +1,22 @@
 <?php
 
-	namespace cron;
+	namespace core\cron\controllers;
 
 	use model\main\Core;
 	use model\main\CoreObject;
 	use Traineratwot\config\Config;
 
-	class CreteAllTable extends CoreObject
+	class CreateAllTable extends CoreObject
 	{
 		function process()
 		{
 			$tables = $this->core->db->getTablesList();
 			chdir(Config::get('BASE_PATH'));
 			foreach ($tables as $table) {
-				exec('wt maketable ' . $table);
+				exec('./wt makeTable ' . $table);
 			}
 		}
 	}
 
 	$core = Core::init();
-	(new CreteAllTable($core))->process();
+	(new CreateAllTable($core))->process();
