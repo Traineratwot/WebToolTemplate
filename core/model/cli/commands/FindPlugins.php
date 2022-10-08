c8ffdc4ccc4b3df17ea7f9aef5050f2d7153c0d7
Kirill <41589091+Traineratwot@users.noreply.github.com>
2022-10-08 20:42:24 +0300

    Update FindPlugins.php

--- core/model/cli/commands/FindPlugins.php
+++ core/model/cli/commands/FindPlugins.php
@@ -21,23 +21,27 @@
 		public function run()
 		{
 			$files   = Utilities::glob(Config::get('BASE_PATH'), '*.php');
-			$count   = count($files);
 			$i       = 0;
 			$plugins = [];
-			foreach ($files as $file) {
-				$i++;
-
+			foreach ($files as $key => $file) {
 				if (stripos($file, 'vendor') !== FALSE) {
-					continue;
+					unset($files[$key]);
 				}
+			}
+			$count   = count($files);
+			foreach ($files as $file) {
+				$found = FALSE;
+				$i++;
 				$content = file_get_contents($file);
 				if (preg_match_all("@Event::emit\(['\"`](\w+)['\"`]@iu", $content, $match)) {
 					foreach ($match[1] as $plugin) {
+						$found                       = TRUE;
 						$plugins[$plugin]['files'][] = str_replace(Config::get('BASE_PATH'), '', $file);
 					}
-				};
-				Console::progress('FindPlugins', $i, $count);
-			};
+				}
+				Console::progress('FindPlugins', $i, $count, $found ? 'green' : 'white');
+			}
+			Console::progress('FindPlugins', $count, $count);
 			echo "\n";
 			foreach ($plugins as $plugin => $data) {
 				Console::info($plugin . ": [" . implode(', ', $data['files']) . ']');
