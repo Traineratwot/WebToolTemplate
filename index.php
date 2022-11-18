5cc9f5d9ebbadef49019f0f11f1082fb5ee7bcda
Kirill <traineratwot@yandex.ru>
2022-11-18 11:54:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 	use model\main\Core;
 	use Traineratwot\config\Config;
 
-	session_start();
 	require_once __DIR__ . '/vendor/autoload.php';
 	Core::init();
 	require_once Config::get('MODEL_PATH') . 'page/Router.php';
